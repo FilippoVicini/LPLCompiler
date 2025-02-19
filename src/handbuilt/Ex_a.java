1f0b509d52b43e1207ceb315e1a8d505e711acab
FilippoVicini <filippo.vicini2@gmail.com>
2025-02-19 09:46:41 +0000

    task 1 ex1, ex2

--- src/handbuilt/Ex_a.java
+++ src/handbuilt/Ex_a.java
@@ -8,7 +8,22 @@ import java.util.ArrayList;
 public class Ex_a {
 
     public static Program buildAST() {
-        return null;
+        List<VarDecl> decls = new ArrayList<>();
+        decls.add(new VarDecl(new TypeInt(), "x"));
+
+        List<Stm> stms = new ArrayList<>();
+        // create int to assign to x
+        Exp a = new ExpInt(3);
+        // assign 3 to x
+        stms.add(new StmAssign("x", a));
+        // multiply x * 9
+        Exp e = new ExpTimes(new ExpInt(9), new ExpVar("x"));
+        // print the last statement
+        stms.add(new StmPrint(e));
+
+        // object program
+        Program program = new Program(decls, stms);
+        return program;
     }
 
     public static void main(String[] args) {
--- src/handbuilt/Ex_b.java
+++ src/handbuilt/Ex_b.java
@@ -8,7 +8,31 @@ import java.util.List;
 public class Ex_b {
 
     public static Program buildAST() {
-        return null;
+        List<VarDecl> decls = new ArrayList<>();
+        // define "part" and "y" variables
+        decls.add(new VarDecl(new TypeInt(), "part"));
+        decls.add(new VarDecl(new TypeInt(), "y"));
+
+        List<Stm> stms = new ArrayList<>();
+        // first statement to add 6 and y
+        Exp a = new ExpPlus(new ExpInt(6), new ExpVar("y"));
+        // assign statment to variable part
+        stms.add(new StmAssign("part", a));
+        // subtract varibale previously defined and 3
+        Exp e = new ExpMinus( new ExpVar("part"), new ExpInt(3));
+        // assign it to variable y
+        stms.add(new StmAssign("y", e));
+        // first part of the print statement
+        Exp c = new ExpPlus(new ExpVar("part"),new ExpVar("y"));
+        // compose print statement
+        Exp d = new ExpTimes(c,new ExpInt(10));
+        // print 
+        stms.add(new StmPrintChar(d));
+
+        Program program = new Program(decls, stms);
+        return program;
+
+
     }
 
     public static void main(String[] args) {
